0ce1d1487062562258420ea5824531a5624911bb
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2019-03-24 17:28:39 -0300

    non generic

---
+++ src/TP_1/Exercise_2/RandomArray.java
@@ -0,0 +1,16 @@
+package TP_1.Exercise_2;
+
+import java.util.Random;
+
+public class RandomArray {
+
+    static int[] randomArray(int n){
+        Random random = new Random();
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        return array;
+    }
+}
